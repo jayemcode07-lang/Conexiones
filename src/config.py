07c863dbd9fdbb826b455e66f88b2da52018650e
Joel <jayemcode07@gmail.com>
2026-06-10 14:38:04 -0500

    Agregando un archivo .gitignore y modificando las variables de entorno

--- src/config.py
+++ src/config.py
@@ -1,6 +1,10 @@
-# Configuración de la base de datos (Variables de entorno)
-DB_HOST = "localhost"
-DB_NAME = "DB_prueba"
-DB_USER = "postgres"
-DB_PASSWORD = "psql2004"
-DB_PORT = "5432"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
